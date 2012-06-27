26e9488190bff6d11d041cdd7fd0693d63a781e6
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-06-27 09:03:10 +0200

    Cleanup and separation of parsing of the motionCommands

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -55,30 +55,10 @@ void Task::updateHook()
         _modem_out.write(std::string(buff));
 
 
-	std::vector<uint8_t> result;
-	boost::circular_buffer<uint8_t>::iterator end = communication::Communication::removeBitSync(buffer,result);
-	buffer.erase(buffer.begin(),end);
-	
-	if(result.size() == 4){
-		//Assuming nurc protokoll
-		bool identifier = result[0]&0x01; //fist bit identifier
-		if(identifier == 1){ //message from nurc
-			double heading = ((result[0] | result[1]<<8) >>1)&0x3FF;
-			heading = ((heading/(double)0x3FF)*(2.0*M_PI)-M_PI);
-			double depth = (result[1] | result[2]<<8)>>2&0x0F;
-			depth= (depth/(double)0x0F)*10.0;
-		    	base::AUVMotionCommand motion_command;
-		    	motion_command.heading = heading;
-		    	motion_command.z = -depth;
-		    	motion_command.x_speed = (result[2]/(double)0xFF)*3.0-1.5;
-		    	motion_command.y_speed = (result[2]/(double)0xFF)*3.0-1.5;
-		    	_motion_command.write(motion_command);
-		}else{
-			std::cerr << "Got invalid package" << std::endl;
-		}
-	}else{
-		printf("Cold not parse packet with undknown length of: %lu\n",result.size());
-	}      	
+	base::AUVMotionCommand motion_command;
+        if(communication::Communication::getMotionCommandfromMessage(buffer,motion_command)){
+             _motion_command.write(motion_command);
+        }
     }
 
      std::string string;
@@ -95,41 +75,11 @@ void Task::updateHook()
      base::samples::RigidBodyState position_samples;
      while(_position_samples.read(position_samples,false) != RTT::NoData && (base::Time::now() -lastSendTime).toSeconds() > _sendInterval.get()){
 	lastSendTime = base::Time::now();
-     	uint8_t message[4];
-	message[0] = 1; //first bit is one
-	uint16_t heading = ((position_samples.getYaw()+ M_PI)/(2.0*M_PI))*(double)0x3FF;
-	uint8_t depth =  ((-position_samples.position[2])/10.0)*(double)0x0F;
-	uint8_t posX = ((-position_samples.position[1])/100.0)*(double)0x0F; 	
-	uint8_t posY = ((-position_samples.position[2])/100.0)*(double)0x0F;
-	heading &= 0x3FF;
-	depth &=0x0F;
-	posX &=0x0F;
-	posY &=0x0F;
-        canbus::Message resp;
-	uint32_t* p = (uint32_t*)resp.data;
-	p[0] = heading & (heading<<1) & (depth<<11) & (posX<<16) & (posY<<24);
-        resp.time = base::Time::now();
-        resp.can_id = 0x1E1;
-        resp.size = 4;
-        _canOut.write(resp);
+        canbus::Message resp = communication::Communication::createPacketFromAUV(position_samples);
+         _canOut.write(resp);
      }
 }
 
-#if 0
-int Task::count1s(int number)
-{
-    int count = 0;
-    // TODO: more than 255. in this scenario not relevant
-    for (int i = 255 - 1; i >= 0; --i)
-    {
-        if (number & (1 << i))
-        {
-            count++;
-        }
-    }
-    return count;
-}
-#endif
 // void Task::errorHook()
 // {
 //     TaskBase::errorHook();
