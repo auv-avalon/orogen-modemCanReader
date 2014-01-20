62932537ca6aff06ca9a3fa05b478a4e3bdb2441
auv <auv@avalon>
2014-01-20 10:17:07 +0100

    Current rock adaps.

--- scripts/test.rb
+++ scripts/test.rb
@@ -6,17 +6,23 @@ ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/buil
 Orocos::CORBA.name_service = "192.168.128.50"
 Orocos.initialize
 
-Orocos::Process.spawn 'modem_test' do |p|
+Orocos::run 'modem_test' do
     can = Orocos::TaskContext.get 'can'
     driver = Orocos::TaskContext.get 'modem_test'
     Orocos.log_all_ports
+    can.device = 'can0'
+    can.configure
+    can.start
+   
 
-    can.modem.connect_to driver.canModem
+
+    #can.out.connect_to driver.canModem
     driver.configure
     driver.start
 
     loop do
-    sleep 0.1
+        driver.modem_in.write "TEST"
+        sleep 0.1
     end
 end
 
